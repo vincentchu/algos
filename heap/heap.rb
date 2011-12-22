8108469743412df109b7e8393c13c269b191c20a
Vincent Chu <vincentchu@gmail.com>
2011-12-21 23:54:33 -0800

    Adding comments to heap

--- heap/heap.rb
+++ heap/heap.rb
@@ -1,3 +1,67 @@
+# >> Heap
+# 
+# A Heap or Max heap is a data structure based on a tree that satisfies the
+# Heap property:
+#
+#     If K is a child of J, then val(J) >= val(K)
+#
+# In other words, the value of the parent node is always greater than or equal
+# to the value of its children. Because of this property, the root of the tree
+# will *always* be the largest value stored in the heap.
+#
+# Closely related is the concept of a Min heap, or a heap where the children
+# are always larger than the parent node; in this case, the root node contains
+# the *smallest* value stored in the heap.
+#
+# Basic Heap Operations
+#
+# A heap typically implements the following operations:
+#
+#  - max: Returns the maxmimum value currently stored in the heap
+#  - push: Push a new value into the heap 
+#  - remove_max: Remove the root (max value)
+#
+# Note that after pushing or popping a value, the heap must be rebalanced to
+# satisfy the heap propery.
+#
+# Uses
+#
+# A heap can be used in a variety of places, including sorting algorithms
+# (Heap sort, e.g.), finding large or small values in a large collection of
+# values. The classical application, however, is in a priority queue.
+#
+# Say you have a large number of jobs, each with a priority. If you stuff them
+# into a heap, then the heap ensures that the highest priority item is always
+# at the root. Querying for the object with the highest priority is O(1) or
+# constant time, which means it's always available.
+#
+# Implementation
+#
+# The implementation below implements a heap as a *binary heap*. This is a very
+# nice and elegant implementation of a heap that uses a simple array (@array)
+# to store the values stored in the heap.
+#
+# Briefly, the tree structure (2 children / parent) is represented in the
+# following way (hard to visualize). The root node is always stored at index 0.
+# Given a node, indexed by n, its two children will be 2n+1 and 2n+2. So, the
+# two children of the root are 1 and 2 (2*0+1, 2*0+2). Their children, respectively,
+# are 3 and 4, and 5 and 6.
+#
+# Returning the max is easy; just return the first value in the array. When
+# pushing, just add the value to the end of the array. Then, call up_heapify! to
+# shuffle the newly-added value up the tree until the heap property is satisfied.
+# If the new value is the new maximum, it will get shuffled all the way up the tree
+# until it becomes the new root.
+#
+# When removing the root, or popping, pop off the root node and swap in the very
+# last element of the tree. Since the last element of the tree is probably smaller
+# then elements below it, shuffle it down by calling down_heapify! until the heap
+# propery is again satisfied. Both pushing and popping are O(log n) operations.
+#
+# References
+#
+# http://en.wikipedia.org/wiki/Binary_heap
+
 class Heap
   def initialize
     @array = []
@@ -82,12 +146,16 @@ class Heap
   end
 end
 
+
+# Create an array of integers 1-10, randomly shuffled
 vals = (1..10).to_a.sort_by { rand }
 # => [7, 4, 2, 1, 3, 8, 6, 5, 9, 10]
 
+# Push these values onto a heap
 h = Heap.new
 vals.each {|v| h.push!(v) }
 
+# Pop them off in successing. This should pop them off in reverse-order
 puts 10.times.collect { h.pop! }.inspect
 # => [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
