d95b3ee3b9475eed91f96322f4d9205007866e87
Vincent Chu <vincentchu@gmail.com>
2011-12-28 16:47:36 -0800

    Adding problem on adding numbers using linked lists

---
+++ problems/binary_tree/common_ancestor.rb
@@ -0,0 +1,19 @@
+require "../../data_structures/binary_tree/binary_tree"
+
+# Problem: Common Ancestor of two nodes
+#
+# Design an algorithm and write code to find the first
+# common ancestor of two nodes in a binary tree Avoid
+# storing additional nodes in a data structure
+# NOTE: This is not necessarily a binary search tree
+
+
+def common_ancestor
+end
+
+
+tree = BinaryTree.new(10)
+n1 = tree.root.add_left_child(9)
+n2 = tree.root.add_right_child(8)
+
+
